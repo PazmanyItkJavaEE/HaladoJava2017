c8bce04bb8294e6d995ee9878cae8cda42ed5fcb
bezse <balzsuzsi@gmail.com>
2017-10-25 11:01:41 +0200

    validátorok

---
+++ lab_4/src/main/java/org/ppke/itk/hj/util/PasswordValidator.java
@@ -0,0 +1,22 @@
+package org.ppke.itk.hj.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+
+public class PasswordValidator implements Validator{
+
+	@Override
+	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
